ddee49274a260768e908dd5a400752f9925c48ea
Pamela Guaman <pamefer.95@gmail.com>
2025-06-20 01:09:53 -0400

    add pics and fixe

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { useEffect, useRef, useState } from 'react';
 import principalPic from './static/DSC_0817.jpg';
 import rascacielo from './static/DSC_0892.jpg';
 import nieve from './static/w3.jpg';
+
 // import rascacielo from './static/rascacielo.jpg';
 import anillos from './static/anillos.png';
 import bola from './static/bola.png';
 import lapromesa from './static/lapromesa.mp3';
 import './App.css';
 import Carousel from './Carousel';
+import { motion } from 'framer-motion';
+
 
 const Pause = ({ onPlayerClick }) => {
   return (
@@ -30,6 +33,23 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [hasInteracted, setHasInteracted] = useState(false);
 
+  const text = "¡Nos casamos!";
+  const letters = Array.from(text);
+
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.15,
+      },
+    },
+  };
+
+  const child = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0 },
+  };
   // 🔁 Handles user interaction anywhere to auto-play
   useEffect(() => {
     const handleUserInteraction = () => {
@@ -89,7 +109,18 @@ function App() {
         </div>
 
         <div className="quoteContainer">
-          <p className="quotePhrase">Nos Casamos!</p>
+          <motion.p
+            variants={container}
+            initial="hidden"
+            animate="visible"
+            className="quotePhrase"
+          >
+            {letters.map((char, index) => (
+              <motion.span key={index} variants={child}>
+                {char}
+              </motion.span>
+            ))}
+          </motion.p>
           <p className="dateFirst">09 · 03 · 2025</p>
 
           {/* <p className="quotePhrase">Lo que Dios ha unido</p>
@@ -99,32 +130,68 @@ function App() {
       </div>
 
       <div className="secondSection normalText">
+        <div className="iconos">
+          <motion.img
+            src={anillos}
+            alt="manos"
+            initial={{ opacity: 0, rotate: -10, scale: 0.95 }}
+            whileInView={{ opacity: 1, rotate: 0, scale: 1 }}
+            transition={{ duration: 5.1, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.5 }}
+          />
+        </div>
         <p>Lo que empezó como una amistad creció con el tiempo... se volvió complicidad, amor, y ahora, un para siempre.</p>
         <div >
-          <img src={nieve} alt="manos" className="centralSquare"></img>
+          <motion.img
+            className="centralSquare"
+            src={nieve}
+            alt="manos"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 5 }}
+          />
         </div>
         <p>
-          Con la promesa más sincera: elegirnos cada día
+          Con la promesa más sincera de elegirnos cada día
         </p>
         <p>
           Tenemos el agrado de invitarte a
         </p>
         <div className="nuestraBoda">
-          <p className="cursiveTextMea line-start">Nuestra</p>
-          <p className=" cursiveTextMea line-end">Boda</p>
+
+          <motion.p
+            className="cursiveTextMea line-start"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 5, delay: 0.5 }}
+            whileInView={{ opacity: 1, y: 0 }}
+
+          >
+            Nuestra
+          </motion.p>
+          <motion.p
+            className="cursiveTextMea line-end"
+            initial={{ opacity: 0, y: 20 }}
+            transition={{ duration: 5 }}
+            viewport={{ once: true, amount: 0.7 }}
+            whileInView={{ opacity: 1, y: 0 }}
+
+          >
+            Boda
+          </motion.p>
         </div>
 
-        <p>Te esperamos para ser parte de este instante eterno</p>
         <div >
           <img src={rascacielo} alt="manos" className="centralCircle"></img>
+
         </div>
       </div>
-      <div className="iconos">
-        <img src={anillos} alt="anillos" ></img>
-      </div>
-      <div className="thirdSection normalText">
-        <p className="cursiveTextMea">Ceremonia y Recepción</p>
 
+      <div className="thirdSection normalText">
+        <p className="subtitle cursiveTextMea">Ceremonia y Recepción</p>
+        <div className="iconos">
+          <img src={anillos} alt="anillos" ></img>
+        </div>
         <div>
           <p>Sábado, 13 de septiembre</p>
           <p>4:00 PM</p>
@@ -153,7 +220,7 @@ function App() {
       </div>
 
       <p></p>
-    </div>
+    </div >
   );
 }
 
